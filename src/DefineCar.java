27338da6a58af5ed4c26c18f8753e6c5840dce5c
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-06-10 21:42:54 +0200

    First commit

--- src/DefineCar.java
+++ src/DefineCar.java
@@ -1,22 +1,56 @@
 public class DefineCar {
-	private final int engSize;
-	private final String licPlate;
-	private final String make;
-	private final String model;
-	
+
+	//Crea delle nuove variabili di tipo private, rendendole modificabili solo tramite i metodi setter della classe
+	private  int engSize;
+	private  String licPlate;
+	private  String make;
+	private  String model;
+
+	//Crea un metodo costruttore per assegnare un valore alle variabili tramite i metodi setter
 	public DefineCar(int engSize, String licPlate, String make, String model) {
-		this.engSize = engSize;
-		this.licPlate = licPlate;
-		this.make = make;
-		this.model = model;
+		setEngSize(engSize);
+		setLicPlate(licPlate);
+		setMake(make);
+		setModel(model);
 	}
-	
-	@Override
-	public String toString() {
-		return
-				"Cilindrata: " + engSize +" cc "+
-				", Targa: '" + licPlate + '\'' +
-				", Costruttore: '" + make + '\'' +
-				", Modello: '" + model + '\'';
+
+	//Metodo setter del parametro cilindrata
+	public void setEngSize( int engSize){
+		this.engSize = engSize; //assegna alla variabile globale "engSize" il valore dell'attributo del metodo"
+	}
+
+	//metodo getter del parametro cilindrata
+	public int getEngSize(){
+		return engSize; //Ritorna il valore della variabile "engSize
+	}
+
+	//Metodo getter del parametro targa
+	public String getLicPlate() {
+		return licPlate;
+	}
+
+	//metodo setter del parametro targa
+	public void setLicPlate( String licPlate ) {
+		this.licPlate = licPlate;
+	}
+
+	//metodo getter del parametro costruttore
+	public String getMake() {
+		return make;
+	}
+
+	//metodo setter del parametro costruttore
+	public void setMake( String make ) {
+		this.make = make;
+	}
+
+	//metodo getter del parametro modello
+	public String getModel() {
+		return model;
+	}
+
+	//metodo setter del parametro modello
+	public void setModel( String model ) {
+		this.model = model;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 public class Main {
 	public static void main(String[] args) {
-		DefineCar car1 = new DefineCar(2000, "EA348SK", "Volkswagen", "Golf");
-		DefineCar car2 = new DefineCar(1600, "CC672MA", "Ford", "Fiesta");
-		
-		System.out.println(car1);
-		System.out.println(car2);
+		//Crea due nuovi oggetti car invocando il costruttore della classe DefineCar
+		DefineCar car1 = new DefineCar(2000, "AB123CD", "Volkswagen", "Golf");
+		DefineCar car2 = new DefineCar(1600, "EF456GH", "Ford", "Fiesta");
+
+		//Stampa gli elementi degli oggetti car invocando i metodi getter della classe DefineCar
+		System.out.println("Auto 1");
+		System.out.println("Cilindrata: " + car1.getEngSize());
+		System.out.println("Targa: " + car1.getLicPlate());
+		System.out.println("Produttore: " + car1.getMake());
+		System.out.println("Modello: " + car1.getModel());
+
+		System.out.println();
+
+		System.out.println("Auto 2");
+		System.out.println("Cilindrata: " + car2.getEngSize());
+		System.out.println("Targa: " + car2.getLicPlate());
+		System.out.println("Produttore: " + car2.getMake());
+		System.out.println("Modello: " + car2.getModel());
+
 	}
 }
